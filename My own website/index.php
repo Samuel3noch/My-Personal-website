2acb373fad39010afb91cb5d878c44f5e6e5fce5
Samuel3noch <1029344@mborijnland.nl>
2022-12-22 13:18:47 +0100

    Main

---
+++ My own website/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="nl"> <!-- Dit is de Taal waarin ik codeer. -->
+<head> <!-- Begin van mijn head. -->
+     <?php include 'Database.php'; ?> <!-- ik heb mijn database in mijn index geplaatst. -->
+     <meta charset="UTF-8"> <!-- Universieele meta van alle europe talen. -->
+     <meta name="Homepage" content="SemPortofolio"> <!-- Welke pagina zitten we op. -->
+     <meta name="keywords" content="HTML, CSS"> <!-- key woord waar me website uit bestaat. -->
+     <meta name="Author" content="SemNfumuntoto"> <!-- De autheur van de website. -->
+     <link rel="stylesheet" href="styleindex.css"> <!-- Hier word mijn website gelinkt aan de stylesheet styleindex.css. -->
+     <title> Home Page </title> <!-- Title van mijn webpagina die boven op de website te zien krijgt. -->
+</head> <!-- Einde van mijn head. -->
+<body> <!-- Begin van mijn body. -->
+ <header> <!-- Begin van mijn header. -->
+     <h1> Logo </h1> <!-- Mijn logo gaat hier plaats vinden. -->
+ </header> <!-- Einde van mijn header. -->
+ <div class="navibar"> <!-- Een div voor mijn navi bar. -->
+ <nav> Tabblad <span class="navspace"></span> Tabblad <span class="navspace"></span> Tabblad </nav> <!-- navigatie met een space ertussen gemaakt door middel van een span.-->
+ </div> <!-- Einde van mijn div voor mijn navi bar. -->
+ <middel class="middel" alt="hier staat alles tussen de {header en footer} "> <!-- Begin midden stuk. (Een midden stuk om wat over mij zelf neer te zetten.) -->
+     <div class="tekst1" alt="Mijn profiel tekst"> <!-- Begin van mijn div. (Een div om mijn  tekst goed neer te zetten.) -->
+         <h1> 
+             TITEL 
+         </h1> 
+         <p> 
+             Hey, leuk dat je dit leest
+         </p>
+     </div> <!-- Einde van mijn div. -->
+     <img src="Image-all/noimage.jpg" alt="noimage" class="noimage"> <!-- Moet een image van mijzelf worden. -->
+ </middel> <!-- Einde van mijn midden stuk. -->
+
+ <comments>
+     <?php 
+      
+    
+     ?>
+    
+ </comments>
+ <footer> <!-- Begin van mijn footer. -->
+ </footer> <!-- Einde van mijn footer. -->
+</body> <!-- Einde van mijn body. -->
+</html>
